1dd8d5b60ac6d8e70eb09cd8465e11e00838ac0a
Pandav Gupta <pandavgupta10@gmail.com>
2021-10-10 12:25:01 +0530

    find range using less comparison method

--- Arrays/range_of_array.cpp
+++ Arrays/range_of_array.cpp
@@ -1,6 +1,8 @@
 #include<iostream>
 using namespace std;
 
+
+/*
 void merge( int* arr , int start, int mid, int last)
 {
     int arr2[last], i, j, k;
@@ -73,6 +75,98 @@ int main(){
     merge_sort( arr , 0 , size-1 );
 
     cout<<"range of array is  "<<arr[size-1]-arr[0];
+
+    delete[] arr;
+    
+
+    return 0;
+    
+}
+*/
+struct Pair
+{
+   int max;
+   int min;
+};
+
+void swap(int* arr, int i, int j){
+    int tmp=arr[i];
+    arr[i]=arr[j];
+    arr[j]=tmp;
+    return;
+}
+
+
+struct Pair find_range(int * arr, int size){
+    struct Pair min_max;
+
+         if ( size/2==0)
+        {
+           if(arr[1]> arr[0]){
+
+                min_max.max=arr[1];
+                min_max.min=arr[0];
+            }
+
+            else{
+
+                min_max.max=arr[1];
+                min_max.min=arr[0];
+
+            }
+        }
+        else {
+              min_max.max=arr[0];
+              min_max.min=arr[0];
+
+        }
+
+        for (int i = 2; i < size-1; i=i+2)
+        { 
+            if ( arr[i] > arr[i+1])
+            {
+                if(arr[i] > min_max.max )
+                    min_max.max=arr[i];
+                if(arr[i+1] < min_max.min )
+                    min_max.min=arr[i+1];
+                
+            }
+            else{
+                if(arr[i+1] > min_max.max )
+                    min_max.max=arr[i+1];
+                if(arr[i] < min_max.min )
+                    min_max.min=arr[i];
+                
+            }
+            
+            
+        }
+        
+   return min_max; 
+    
+}
+
+
+
+
+int main(){
+    int size,tmp;
+    int* arr=NULL;
+    struct Pair min_max;
+    
+    cout<<"Enter the size of array: ";
+    cin>>size;
+    arr= new int[size];
+    
+    cout<<"Enter the element of array: ";
+    for ( int i = 0 ; i < size ; i++)
+    {
+        cin >> arr[i];
+    }
+
+    min_max=find_range( arr , size );
+
+    cout<<"range of array is  "<<min_max.max-min_max.min;
 
     delete[] arr;
     
