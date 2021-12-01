5b073633efd772061c9a94dd34948f62e151b119
Pandav Gupta <pandavgupta10@gmail.com>
2021-12-02 00:46:17 +0530

    check interleaving of string with distinct element

--- Strings/Intermediate Questions/check_string_interleaving.cpp
+++ Strings/Intermediate Questions/check_string_interleaving.cpp
@@ -4,69 +4,22 @@ using namespace std;
 
 
 bool string_interleaving(string s1,string s2,string s3){
-    string first,second;
-    int i=0,j=0;
-
-    if(  s3.size()  !=  s1.size()+s2.size() )
-    return false;
-
-
-    if( s3[0]==s1[0] || s3[0]==s2[0]){
-        if(s2[0]==s1[0]){
-            i++;
-            j++;
-            while(s1[i]==s2[j]){
-                i++;
-                j++;
-            }
-               
-                                if( s3.substr(0,i+1)==s1.substr(0,i+1) ){
-                                  string first=s1;
-                                   string second=s2;
-
-                                }
-                                else{
-                                   string first=s2;
-                                   string second= s1;
-
-                                }
-            
-
-        }
-        else if(s3[0]==s1[1]){
-           string first= s1;
-           string second= s2;
-
-        }
-        else{
-          string first=s2;
-          string second=s1;
-
-        }
-    }
-    else
-        return false;
-
-
-   j=0;
-   int k=0;
-    for (int i = 0; i < s3.length(); i++){
-        if( s3[i] != first[j] )
-        return false;
-        while (first[i]==s3[i])
-        {
-            i++;
+    if(s3.size()!= s1.size()+s2.size())
+      return false;
+    int j=0,k=0;
+    for (int i = 0; i < s3.size(); i++)
+    {
+        if(s1[j]=s3[i]){
             j++;
         }
-        if(second[k]!=s3[i])
-        return false;
-        while (second[i]==s3[i])
-        {
-            i++;
+        else if(s2[k]=s3[i]){
             k++;
+           }
+        else{
+            return false;
         }
-        
     }
+
 
     return true;
 
