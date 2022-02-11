24ebf2a9ea1ce7de601056c5e96cd4fc8cf32f53
Pandav Gupta <pandavgupta10@gmail.com>
2022-02-11 20:14:58 +0530

    updating detect loop with create loop option

--- Linked_list/detect_loop.cpp
+++ Linked_list/detect_loop.cpp
@@ -20,7 +20,25 @@ void print_list(ListNode* headptr){
      cout<<(headptr->key)<<" ";
      headptr= headptr->next;
   }
+  cout<<"\n";
+  return;
   
+}
+
+void  create_loop(ListNode* head, int index_first){
+    ListNode* it = head;
+    ListNode* prev=nullptr;
+    int count=0;
+    while (it->next!=nullptr)
+    {
+        if(index_first==count){
+          prev = it;
+        }
+        it=it->next;
+        count++;
+    }
+    it->next=prev;
+    return ;
 }
 
 bool detect_loop(ListNode* head){
@@ -47,9 +65,11 @@ int main(){
     }
     cout<<"Element in linked list: ";
     print_list(head);
-    cout<<"\n";
+    // for creating loop
+    cout<<"enter index connect to last index: ";
+    cin>>n;
+    create_loop(head,n);
     cout<<"LOOP is present or not: ";
     cout<<detect_loop(head);
-
     return 0;
 }
